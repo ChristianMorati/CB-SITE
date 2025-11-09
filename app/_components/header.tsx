b11334856fd9c371bd6615158301527b174cb019
christianmorati <christianmorati3@gmail.com>
2025-11-09 20:25:36 -0300

    initial commit

---
+++ app/_components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <nav className="
+            w-[100vw]
+            h-[10vh]
+            bg-white
+            ">
+                Header
+        </nav>
+    )
+}
